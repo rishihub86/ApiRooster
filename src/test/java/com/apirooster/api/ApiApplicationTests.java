49467fb04b02c7125ea7c6453f22c48f79bbf1fd
Saptarshi Banerjee <saptarshi.banerjee86@gmail.com>
2023-02-15 23:20:52 +0530

    Initial Commit

---
+++ src/test/java/com/apirooster/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirooster.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
